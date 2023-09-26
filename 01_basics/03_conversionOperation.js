0fc22f43110f893f2a0de2e6ec7ad56a38561812
Anonymousride <90982862+Anonymousride@users.noreply.github.com>
2023-09-26 17:35:45 +0000

    conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,9 @@
+ let score = 33
+ let scored = "33abc"
+
+ console.log(typeof score);
+ console.log(typeof (scored));
+
+ let valueInNumber = Number(scored)
+ console.log(typeof valueInNumber)
+ console.log(valueInNumber)
