d6f6c3a1b2565f53470e1b9d06586fc5e01e51a7
Anonymousride <90982862+Anonymousride@users.noreply.github.com>
2023-10-01 13:36:33 +0000

    num and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,40 @@
+// const num =400
+// console.log(num);
+
+// const newnum =new Number(100)
+// console.log(newnum);
+// console.log(typeof newnum);
+
+const intege = 500
+// console.log(intege.toString().length);
+// console.log(intege.toFixed(2)); // (show upto decimal 3): Generally used in E-commerce website at the calculation place
+
+// console.log(Number.isInteger(500));
+
+const zeros = 1000000
+// console.log(zeros.toLocaleString());
+// console.log(zeros.toLocaleString('en-In'));
+// console.log(typeof zeros.toLocaleString());
+
+
+// console.log(Number.MAX_SAFE_INTEGER);
+
+//++++++++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(5.6));
+// console.log(Math.round(7.535));
+// console.log(Math.round(7.235));
+// console.log(Math.ceil(7.235));
+// console.log(Math.floor(7.635));
+
+console.log(Math.random()); // random(): function ki value hamesha 0 to 1 ke bech mei hoga
+console.log(Math.random()*10); // maybe a probability of coming zero after multiply by 10 Ex: 0.0035688*10 i.e. 0.035688
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min =10
+console.log((Math.random()*10) + min);
+console.log(Math.floor(Math.random()*10) + min);
