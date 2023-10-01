12620208256e37deaef54968344d643125a6c3bc
Anonymousride <90982862+Anonymousride@users.noreply.github.com>
2023-10-01 12:47:23 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name ="Manish"
+const age= 22
+console.log(`My name is ${name} and my age is ${age}`);
+
+const newname =new String ("Nightshade")
+console.log(newname);
+console.log(newname.__proto__)
+
+console.log(newname.charAt(5));
+
+console.log(newname.length);
+
+console.log(newname.indexOf("g"))
+
+console.log(newname.substring(0,4));
+console.log(newname.substring(3));
+console.log(newname.substring(-2,5))
+
+console.log(newname.slice(3,6));
+console.log(newname.slice(-8,4));
