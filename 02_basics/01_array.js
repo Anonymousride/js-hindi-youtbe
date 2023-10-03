ba8a0bddeea22bef7aa59ef24e72b4cfcdcfc2ed
Anonymousride <90982862+Anonymousride@users.noreply.github.com>
2023-10-03 14:32:12 +0000

    Array_in_JS

---
+++ 02_basics/01_array.js
@@ -0,0 +1,14 @@
+// Array
+
+// const myArr = [10,11,21,73,43,57]
+// console.log(myArr);
+// console.log(myArr[3]);
+// console.log(myArr[7]);
+
+const newArr = new Array(1,2,6,3,4,5)
+// console.log(newArr);
+// console.log(newArr.length);
+// newArr.push(45)
+// console.log(newArr);
+// console.log(newArr.push(45));
+
