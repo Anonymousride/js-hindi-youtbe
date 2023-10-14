3a0fa5184db547a4891f5e1e817b92e9cbd08161
Manish Kumar <90982862+Anonymousride@users.noreply.github.com>
2023-10-14 11:27:42 +0000

    objects2additionalInfo

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,2 +1,8 @@
-const NewUser = new Object()
+// const NewUser = new Object()
+const NewUser = {}
+
+
+// NewUser.id ="123abd"
+// NewUser.name ="Tommy"
+// NewUser.isLoggin = false
 console.log(NewUser);
