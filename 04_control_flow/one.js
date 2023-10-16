bbc8db288ef73d6a537bc4aeee888f80dcb02d29
Manish Kumar <90982862+Anonymousride@users.noreply.github.com>
2023-10-16 09:53:28 +0000

    if else and elseif

---
+++ 04_control_flow/one.js
@@ -0,0 +1,20 @@
+// if(true)
+// {
+//     console.log('Hello,Nightshade');
+// }
+
+// if(true) console.log('Hello,Devil');
+
+//-------------------------------------Else-if---------------------------------------//
+
+if(10<1)
+{
+    console.log('10 is less than 1');
+}
+else if(10>12)
+{
+    console.log('10 is greater than 12');
+}
+else{
+    console.log('Kuch toh gadbar hai!');
+}
